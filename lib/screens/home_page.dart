b9a47da5356430b395898ceb63e8a9921933806f
aya-hamoudeh1 <ayahamoudeh29@gmail.com>
2025-01-18 04:49:31 +0300

    create product card

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -22,6 +22,62 @@ class HomePage extends StatelessWidget {
           "New Trend",
         ),
       ),
+      body: Center(
+        child: Container(
+          height: 130,
+          width: 220,
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 40,
+                color: Colors.grey.withOpacity(.2),
+                spreadRadius: 0,
+                offset: const Offset(10, 10),
+              ),
+            ],
+          ),
+          child: const Card(
+            elevation: 10,
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 16.0,
+                vertical: 16,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "HandBag LV",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 16,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        r"$225",
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                      Icon(
+                        Icons.favorite,
+                        color: Colors.red,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
